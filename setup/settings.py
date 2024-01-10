d4962113663588ec4197a073057bd614d063d03e
Alisson Demys <Alisson_demys@hotmail.com>
2024-01-10 11:44:25 -0300

    fix docker e settings

--- setup/settings.py
+++ setup/settings.py
@@ -43,8 +43,8 @@ MYSQL_ROOT_PASSWORD = os.getenv("MYSQL_ROOT_PASSWORD")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_HOST = os.getenv("MYSQL_HOST")
-MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_HOST = str(os.getenv("MYSQL_HOST"))
+MYSQL_PORT = str(os.getenv("MYSQL_PORT"))
 
 
 # Quick-start development settings - unsuitable for production
