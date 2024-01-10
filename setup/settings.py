63026d634ab0cf95aeba0935a61cc48d71418dff
Alisson Demys <Alisson_demys@hotmail.com>
2024-01-10 14:32:59 -0300

    Adding allowed-hosts

--- setup/settings.py
+++ setup/settings.py
@@ -56,7 +56,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mabeldev.pythonanywhere.com"]
 
 
 # Application definition
