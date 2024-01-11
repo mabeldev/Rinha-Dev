2426a1f64840890dbef0f0a4ae17beb629d2ac7b
Alisson Demys <Alisson_demys@hotmail.com>
2024-01-11 16:28:44 -0300

    fix env

--- setup/settings.py
+++ setup/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/static_files/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "setup/static/")]
 STATIC_ROOT = os.path.join(BASE_DIR, "static_files/")
